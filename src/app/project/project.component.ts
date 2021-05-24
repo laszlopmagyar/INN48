47025accafec49113bbee75353284f4ed7193dbd
Michelle Burton <michelle.burton@fisglobal.com>
2021-05-24 15:54:05 -0500

    Added JSON file of projects

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import projects from './projects.json';
 
 @Component({
     selector: 'app-project',
     templateUrl: './project.component.html'
 })
 export class projectComponent {
-
+    title = "Search";
+    projectList: {
+        name: string,
+        long_description: string,
+        short_description: string,
+        skills: string,
+        current_state: string
+    }[] = projects
 }
