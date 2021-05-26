e3a6856220de0f259c2053a363e6bdb302cf1f2a
Kim, JinWook <jinwook.kim@fisglobal.com>
2021-05-25 21:23:38 -0400

    added more stuffs to app1 and 2

--- src/app/app1/app1.component.ts
+++ src/app/app1/app1.component.ts
@@ -10,6 +10,26 @@ import projects from '../../assets/projects.json';
 })
 export class App1Component implements OnInit {
 
+    selectedOption: string;
+    options = [
+      { name: "Application (FIS)", value: 1 },
+      { name: "Basic", value: 2 },
+      { name: "Computer Languages", value: 3 },
+      { name: "Corporate Finance", value: 4 },
+      { name: "Customer Service", value: 5 },
+      { name: "Human Resources", value: 6 },
+      { name: "Management", value: 7 },
+      { name: "Management Consulting", value: 8 },
+      { name: "Microsoft Skills", value: 9 },
+      { name: "Sales Management", value: 10 },
+      { name: "Systems", value: 11 },
+      { name: "Talent Management", value: 12 },
+      { name: "Technical", value: 11 },
+      { name: "Unix", value: 13 }
+    ]
+  
+
+
   constructor() {
 
    }
--- src/app/app2/app2.component.ts
+++ src/app/app2/app2.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class App2Component implements OnInit {
 
+  //selectedOption: string;
+  //options = [
+  //  { name: "Application (FIS)", value: 1 },
+  //  { name: "Basic", value: 2 },
+  //  { name: "Computer Languages", value: 3 },
+  //  { name: "Corporate Finance", value: 4 },
+  //  { name: "Customer Service", value: 5 },
+  //  { name: "Human Resources", value: 6 },
+  //  { name: "Management", value: 7 },
+  //  { name: "Management Consulting", value: 8 },
+  //  { name: "Microsoft Skills", value: 9 },
+  //  { name: "Sales Management", value: 10 },
+  //  { name: "Systems", value: 11 },
+  //  { name: "Talent Management", value: 12 },
+  //  { name: "Technical", value: 11 },
+  //  { name: "Unix", value: 13 }
+  //]
+
+
   constructor() { }
 
   ngOnInit(): void {
